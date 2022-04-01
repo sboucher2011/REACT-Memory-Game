22a9794d2ca22aef1bd747e070c0a4e6dda8bd16
sboucher <sboucher2011@gmail.com>
2022-03-31 22:37:26 -0400

    image issues

--- src/App.js
+++ src/App.js
@@ -4,11 +4,11 @@ import SingleCard from './components/SingleCard'
 
 const cardImages = [
   { "src": "img/helmet-1.png", matched: false },
-  { "src": "/img/potion-1.png", matched: false },
-  { "src": "/img/ring-1.png", matched: false },
-  { "src": "/img/scroll-1.png", matched: false },
-  { "src": "/img/shield-1.png", matched: false },
-  { "src": "/img/sword-1.png", matched: false },
+  { "src": "img/potion-1.png", matched: false },
+  { "src": "img/ring-1.png", matched: false },
+  { "src": "img/scroll-1.png", matched: false },
+  { "src": "img/shield-1.png", matched: false },
+  { "src": "img/sword-1.png", matched: false },
 ]
 
 function App() {
